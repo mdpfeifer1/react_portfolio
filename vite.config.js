8f8b0997dacf7c8169911fc3e4f48839759fc854
mdpfeifer1 <mikelaidenpfeifer@gmail.com>
2024-11-12 20:49:33 -0600

    deleted yml

--- vite.config.js
+++ vite.config.js
@@ -10,7 +10,7 @@ export default defineConfig({
       "@fortawesome/free-brands-svg-icons",
     ],
   },
-  base: "/react_portfolio",
+  base: "/react_portfolio/",
   plugins: [react()],
   server: {
     proxy: {
