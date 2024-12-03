0ded9637bbd3d52a87b589837a6794bec650a6be
mdpfeifer1 <mikelaidenpfeifer@gmail.com>
2024-12-03 13:20:47 -0600

    updated resume

--- src/main.jsx
+++ src/main.jsx
@@ -2,14 +2,13 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App.jsx";
 import "./index.css";
-// Bringing in the required imports from 'react-router-dom' to set up application routing behavior
 import { createHashRouter, RouterProvider } from "react-router-dom";
 import Contact from "./pages/Contact.jsx";
 import About from "./pages/About.jsx";
 import Work from "./pages/Work.jsx";
 import Resume from "./pages/Resume.jsx";
 
-// Define the accessible routes, and which components respond to which URL
+
 const router = createHashRouter([
   {
     path: "/",
--- vite.config.js
+++ vite.config.js
@@ -1,12 +1,9 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 
-// https://vitejs.dev/config/
 export default defineConfig({
-  // Include PNG assets
   assetsInclude: ["**/*.PNG", "**/*.png"],
 
-  // Include necessary dependencies
   optimizeDeps: {
     include: [
       "@fortawesome/react-fontawesome",
@@ -16,13 +13,11 @@ export default defineConfig({
 
   // Plugins for React
   plugins: [react()],
-  // Adjust base for deployment in subdirectories (use your own path)
+
   base: "/react_portfolio/",
 
-  // Build configuration to ensure output directory and filenames are correct
   build: {
-    outDir: "dist", // Ensure output goes to the dist folder
-    // We don't need to manually define the entry here, Vite should handle it automatically
+    outDir: "dist",
   },
 
   // Server proxy for development
