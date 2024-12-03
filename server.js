33d046201bdba3c5394704db9b4d437cccb01c3a
mdpfeifer1 <mikelaidenpfeifer@gmail.com>
2024-12-03 14:19:15 -0600

    updated resume

--- server.js
+++ server.js
@@ -20,7 +20,7 @@ app.use(bodyParser.json());
 app.use(
   cors({
     origin: "https://mikelpfeiferportfolio-9ef30b755f95.herokuapp.com/",
-    "https://mdpfeifer1.github.io/react_portfolio/",
+    // "https://mdpfeifer1.github.io/react_portfolio/",
     methods: ["GET", "POST", "OPTIONS"],
   })
 );
